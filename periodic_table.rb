fa7dde4c06773c8dce4de6bb6312c64252cb693a
NC <nc@ncs-macbook-pro.ait-sydney.net.au>
2018-08-24 12:29:22 +1000

    Created periodic_table.rb with application requirements

---
+++ periodic_table.rb
@@ -0,0 +1,22 @@
+#################################################################### ‎(ﾉಥ益ಥ）ﾉ ┻━┻
+#   Requirements =================================================##
+#   - Your application must have a class named Element with      |## 
+#       the following attributes:                                |##
+#           - name                                               |## (╯'□')╯︵ ┻━┻
+#           - atomic weight                                      |##
+#           - atomic number                                      |##
+#                                                                |##
+#   - Find THREE elements on the periodic table and record       |## (ﾉಥДಥ)ﾉ︵┻━┻･/
+#     their name, atomic weight and atomic number                |##
+#                                                                |##
+#   - Implement a way to round the atomic weight of the element  |##
+#     to the nearest whole numner                                |## ʕノ•ᴥ•ʔノ ︵ ┻━┻
+#                                                                |##
+#   - Implement a way to display your three elements and all     |##
+#     their attributes, including their atomic weight rounded    |##
+#     to the nearest whole number                                |## ┬─┬ ︵ /(.□. \）
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - ##
+####################################################################
+#################################################################### (ノ^_^)ノ┻━┻ ┬─┬ ノ( ^_^ノ)
+
+### YOUR CODE HERE:
