4ffdb3d477e25a7d1e5fcf0504d03c0ae4842b34
Nick Clark <nick.clark@coderacademy.edu.au>
2018-08-30 12:14:22 +1000

    Added requirements for class instance methods and variables

--- periodic_table.rb
+++ periodic_table.rb
@@ -1,16 +1,24 @@
 #################################################################### ‎(ﾉಥ益ಥ）ﾉ ┻━┻
 #   Requirements =================================================##
-#   - Your application must have a class named Element with      |## 
+#   - Your application must have a class named Element with      |##
 #       the following attributes:                                |##
 #           - name                                               |## (╯'□')╯︵ ┻━┻
 #           - atomic weight                                      |##
 #           - atomic number                                      |##
+#     Put your Element class in a separate file to               |##
+#     periodic_table.rb (require_relative)                       |##
+#                                                                |##
+#     - Create class instance methods and a class                |##
+#       instance variable:                                       |##
+#           - return all instances of the Element class          |##
+#           - return a count of Element instances                |##
 #                                                                |##
 #   - Find THREE elements on the periodic table and record       |## (ﾉಥДಥ)ﾉ︵┻━┻･/
-#     their name, atomic weight and atomic number                |##
+#     their name, atomic weight and atomic number and            |##
+#     instantiate them as instances of your Element class        |##
 #                                                                |##
 #   - Implement a way to round the atomic weight of the element  |##
-#     to the nearest whole numner                                |## ʕノ•ᴥ•ʔノ ︵ ┻━┻
+#     to the nearest whole number                                |## ʕノ•ᴥ•ʔノ ︵ ┻━┻
 #                                                                |##
 #   - Implement a way to display your three elements and all     |##
 #     their attributes, including their atomic weight rounded    |##
